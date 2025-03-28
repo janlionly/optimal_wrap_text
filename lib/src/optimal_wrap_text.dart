8f7d6e9c9165dbafaa3ac64a506173ef423cbf54
Chris Rutkowski <chris@rkw.ski>
2025-03-29 00:49:55 +0700

    adds trivial documentation

--- lib/src/optimal_wrap_text.dart
+++ lib/src/optimal_wrap_text.dart
@@ -2,24 +2,49 @@ import 'package:flutter/material.dart';
 
 import 'optimal_wrap_text_for_width.dart';
 
+/// A widget that wraps text optimally.
 final class OptimalWrapText extends StatelessWidget {
   /// Provide the width for edge cases where LayoutBuilder cannot be used.
   /// Such as inside SliverFillRemaining
   final double? width;
 
+  /// See equivalent property in [Text]
   final String text;
+
+  /// See equivalent property in [Text]
   final TextStyle? style;
+
+  /// See equivalent property in [Text]
   final StrutStyle? strutStyle;
+
+  /// See equivalent property in [Text]
   final TextAlign? textAlign;
+
+  /// See equivalent property in [Text]
   final TextDirection? textDirection;
+
+  /// See equivalent property in [Text]
   final Locale? locale;
+
+  /// See equivalent property in [Text]
   final bool? softWrap;
+
+  /// See equivalent property in [Text]
   final TextScaler? textScaler;
+
+  /// See equivalent property in [Text]
   final String? semanticsLabel;
+
+  /// See equivalent property in [Text]
   final TextWidthBasis? textWidthBasis;
+
+  /// See equivalent property in [Text]
   final TextHeightBehavior? textHeightBehavior;
+
+  /// See equivalent property in [Text]
   final Color? selectionColor;
 
+  /// Creates a widget that wraps text optimally.
   const OptimalWrapText(
     this.text, {
     super.key,

b7d3eb6965236406a1d8e1a793078076be484e63
Chris Rutkowski <chris@rkw.ski>
2025-03-29 16:08:47 +0700

    adds dummy example app (just main.dart)

---
+++ example/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:optimal_wrap_text/optimal_wrap_text.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+final class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('OptimalWrapText Example')),
+        body: Center(
+          child: OptimalWrapText(
+            'My slightly longer text that will span 2 or 3 lines',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 18),
+          ),
+        ),
+      ),
+    );
+  }
+}
